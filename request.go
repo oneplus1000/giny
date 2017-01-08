60ba764fba63315a18e2c20ed8df56f791abd9d2
oneplus1000 <oneplus1000@gmail.com>
2017-01-08 23:29:51 +0700

    add func ReadJSON and ReadString

--- request.go
+++ request.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 //ReadStringFromBody read string from http body
@@ -28,4 +30,14 @@ func ReadJSONFromBody(r *http.Request, obj interface{}) error {
 		return err
 	}
 	return nil
+}
+
+//ReadJSON read json from http body
+func ReadJSON(ctx gin.Context, obj interface{}) error {
+	return ReadJSONFromBody(ctx.Request, obj)
+}
+
+//ReadString read string fomr http body
+func ReadString(ctx gin.Context) (string, error) {
+	return ReadStringFromBody(ctx.Request)
 }
